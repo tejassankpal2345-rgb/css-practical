99e55e2fbaea6aa95113cb6fa56624d8bcb0445e
tejassankpal2345-rgb <tejassankpal2345@gmail.com>
2026-04-03 03:38:09 -0700

    Delete Practical_12 directory

--- Practical_12/Basic Controller/app.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module("myApp", []);
-
-app.controller("BasicCtrl", function($scope) {
-    $scope.message = "Hello AngularJS!";
-    $scope.greet = function() {
-        alert("Welcome!");
-    }
-});
